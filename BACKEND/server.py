3a14c74009f7fe4813d6b657dacad974fae68010
boujrada yassine <63125972+yassineboujrada@users.noreply.github.com>
2023-02-16 00:37:00 +0100

    pymongo with dammy data

--- BACKEND/server.py
+++ BACKEND/server.py
@@ -1,16 +1,45 @@
-from flask import Flask,render_template,redirect,session,request
+from flask import Flask,render_template,redirect,session,request,jsonify
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from bson.objectid import ObjectId
 
+load_dotenv()
+import os
+
+URL_LINK = os.getenv("DB_URL")
 app = Flask(__name__)
 app.secret_key = "maked by yassine boujrada"
 
-@app.route('/data')
+cluster = MongoClient(URL_LINK)
+db = cluster["stock"]
+
+@app.route('/prod_data')
 def dash():
-    return {
-        'Name':"geek", 
-        "Age":"22",
-        "Date":"jdii", 
-        "programming":"python"
-        }
+    l=[]
+    result = db["product"].find({})
+    for docs in list(result):
+        docs["_id"] = str(docs["_id"])
+        l.append(docs)
+    return jsonify(list(l))
+
+@app.route('/order_data')
+def order():
+    l=[]
+    result = db["orders"].find({})
+    for docs in list(result):
+        docs["_id"] = str(docs["_id"])
+        l.append(docs)
+    return jsonify(list(l))
+
+@app.route('/costumer_data')
+def costumer():
+    l=[]
+    result = db["costumers"].find({})
+    for docs in list(result):
+        docs["_id"] = str(docs["_id"])
+        l.append(docs)
+    return jsonify(list(l))
 
 if __name__=="__main__":
     app.run(port="3001",debug=True)
